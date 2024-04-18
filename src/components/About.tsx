87b30f20368ac8adc4a8c39663e4edb35326a484
María Torrente <caritotorrente@gmail.com>
2024-04-18 19:16:49 -0400

    Updating about text.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,14 @@ export default function About() {
   return (
     <Stack spacing={1} color="info.main">
       <Typography fontSize={[16, 18]}>
-        I'm a curious being, who loves challenges, people and music...
+        In the solo dance of technology and music, I find my rhythm, weaving
+        code and melodies into seamless experiences that strike a chord with
+        users. With a passion for innovation and a knack for human connection, I
+        orchestrate harmonious solutions that bridge the gap between the digital
+        realm and the dance floor of life. Let's harmonize together,
+        transforming melodies into great designs, beats into rhythmic
+        development cycles, and progressions into solid infrastructures that
+        support lasting user experiences.
       </Typography>
       <SpotifyNowPlaying />
     </Stack>
