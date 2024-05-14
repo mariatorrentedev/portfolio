523537c9a76cfec4d0a2c00663030e10b42335b8
María Torrente <caritotorrente@gmail.com>
2024-05-14 15:30:37 -0400

    develop: simple footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,19 @@
 import { Typography, Container, Stack, IconButton } from "@mui/material";
-import MT from "../assets/mt.png";
+import MTLogo from "../assets/mt.png";
 
 export default function Footer() {
   return (
-    <Container component="footer" maxWidth="md" sx={{ padding: 3 }}>
+    <Container component="footer" maxWidth="md" sx={{ padding: 2 }}>
       <Stack alignItems="center" spacing={1}>
-        Built with Vite, React, Typescript and Material UI, deployed to Pages.
         <IconButton
           component="a"
           color="secondary"
           sx={{ img: { maxWidth: 50 } }}
           href="#"
-          rel="noopener noreferrer"
         >
-          <img src={MT} alt="Maria Torrente" />
+          <img src={MTLogo} alt="Maria Torrente" />
         </IconButton>
-        <Typography variant="caption"> Maria Torrente 2024</Typography>
+        <Typography variant="caption"> © Maria Torrente 2024</Typography>
       </Stack>
     </Container>
   );
