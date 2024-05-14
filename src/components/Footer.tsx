67070ecb9a1294b40e240c8ced9d07dc74311318
Maria Torrente <64274966+mariatorrentedev@users.noreply.github.com>
2024-05-14 15:37:29 -0400

    Minor footer update (#30)
    * Some clean up, added Spotify icon for offline/loading state
    
    * Minor clean up in Mobile view
    
    * Updating about text.
    
    * Fixing max width for the Tab Panel
    
    * P-18: fixed bug when type is not track
    
    * P-20: Add real time spotify streaming progress bar. (#21)
    
    * P-23: Added experience section. (#24)
    
    * P-26: Added placeholder projects section. (#27)
    
    * Wip footer.
    
    * develop: some footer updates, brb.
    
    * develop: simple footer.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import { Typography, Container, Stack, IconButton } from "@mui/material";
+import MTLogo from "../assets/mt.png";
+
+export default function Footer() {
+  return (
+    <Container component="footer" maxWidth="md" sx={{ padding: 2 }}>
+      <Stack alignItems="center" spacing={1}>
+        <IconButton
+          component="a"
+          color="secondary"
+          sx={{ img: { maxWidth: 50 } }}
+          href="#"
+        >
+          <img src={MTLogo} alt="Maria Torrente" />
+        </IconButton>
+        <Typography variant="caption"> © Maria Torrente 2024</Typography>
+      </Stack>
+    </Container>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Navigation } from "./Navigation";
 export { default as Experience } from "./Experience";
 export { default as Projects } from "./Projects";
 export { default as SpotifyNowPlaying } from "./SpotifyNowPlaying";
+export { default as Footer } from "./Footer";
