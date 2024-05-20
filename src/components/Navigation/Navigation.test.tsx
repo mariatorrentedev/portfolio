9eed919c03a43cd1b97f1e6a08d719b1725a1ca8
Maria Torrente <64274966+mariatorrentedev@users.noreply.github.com>
2024-05-20 18:04:21 -0400

    Update src/components/Navigation/Navigation.test.tsx

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -2,7 +2,7 @@ import { describe, test } from "vitest";
 import { renderWithQueryProvider } from "../../utils.tests";
 import Navigation from "./Navigation";
 
-// TODO: understand MUI error with `.main` usages.
+// TODO: understand MUI error with `theme` usages, mainly custom properties.
 describe.skip("<Navigation />", () => {
   test("Displays proper information per project", () => {
     renderWithQueryProvider(<Navigation />);
